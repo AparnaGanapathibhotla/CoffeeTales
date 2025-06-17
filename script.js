523a28a2aad81c73ebe9a7fca1d66178b98a2f7a
AparnaGanapathibhotla <aparna16692@gmail.com>
2025-06-17 07:40:41 +0530

    coffee tailes

---
+++ script.js
@@ -0,0 +1,31 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+//Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () =>  menuOpenButton.click
+());
+
+//Close menu when the nav link is clicked
+navLinks.forEach(link => {
+    link,addEventListener("click", () => menuOpenButton.click());
+});
+
+//Initialize Swiper
+const Swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+    //If we need pagination 
+    pagination: {
+        el:'.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    }
+})
